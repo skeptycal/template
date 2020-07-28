865e1a987b2ab628c1a3df1c70246daf5f077ec8
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-07-27 22:22:44 -0500

    # (GitIt Bot) minor updates and formatting # https://www.github.com/skeptycal

---
+++ static/packaging.python.org/js/readthedocs-data.js
@@ -0,0 +1,21 @@
+var READTHEDOCS_DATA = {
+    project: "python-packaging-user-guide",
+    version: "latest",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "http://packaging.python.org/",
+    theme: "pypa_theme",
+    builder: "sphinx",
+    docroot: "/source/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+
+    proxied_api_host: "/_",
+
+    commit: "dd99ee87",
+    ad_free: false,
+
+    global_analytics_code: "UA-17997319-1",
+    user_analytics_code: null
+};
